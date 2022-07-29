570abc3b23abee254d485b1ca8a52d2856bf9bb3
edigonzales <edi.gonzales@gmail.com>
2022-07-29 19:18:21 +0200

    clean code

--- sodata-server/src/main/java/ch/so/agi/sodata/Application.java
+++ sodata-server/src/main/java/ch/so/agi/sodata/Application.java
@@ -5,19 +5,24 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.ForwardedHeaderFilter;
 
 @SpringBootApplication
 @ServletComponentScan
-public class Application
-    extends SpringBootServletInitializer {
+public class Application extends SpringBootServletInitializer {
   
-  public static void main(String[] args) {
-    SpringApplication.run(Application.class, args);
-  }
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
   
-  @Override
-  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-    return builder.sources(Application.class);
-  }
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
+    }
   
+    @Bean
+    public ForwardedHeaderFilter forwardedHeaderFilter() {
+        return new ForwardedHeaderFilter();
+    }
 }
