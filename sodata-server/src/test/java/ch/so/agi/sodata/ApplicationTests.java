cff7281842f9a6bd9847513256a904dbeb851da3
Stefan Ziegler <edi.gonzales@gmail.com>
2023-02-21 16:12:30 +0100

    Native compile does not work. Commit and push but move ilidata stuff to new Spring Boot 3 app.

--- sodata-server/src/test/java/ch/so/agi/sodata/ApplicationTests.java
+++ sodata-server/src/test/java/ch/so/agi/sodata/ApplicationTests.java
@@ -33,6 +33,25 @@ class ApplicationTests {
     @Test
     public void search_Ok() throws Exception {
         assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/themepublications?query=afu", String.class))
-                .contains("fliess");
+                .contains("Abbaustellen");
     }
+    
+    @Test
+    public void ilisite_Ok() throws Exception {
+        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/ilisite.xml", String.class))
+        .contains("IliSite09.SiteMetadata.Site");        
+    }
+    
+    @Test
+    public void ilimodels_Ok() throws Exception {
+        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/ilimodels.xml", String.class))
+        .contains("IliRepository09.RepositoryIndex");        
+    }
+
+    @Test
+    public void ilidata_Ok() throws Exception {
+        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/ilidata.xml", String.class))
+        .contains("DatasetIdx16.DataIndex.DatasetMetadata");        
+    }
+
 }
